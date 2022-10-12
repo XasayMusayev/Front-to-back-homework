b6417a2bbd473872088cbe51db2dfa771813edc9
Xasay Musayev <xasaymusayev01@gmail.com>
2022-10-12 19:51:27 +0400

    Add project files.

---
+++ Front-to-back-homework/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Front_to_back_homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
